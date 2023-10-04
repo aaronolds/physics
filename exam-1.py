7dd8c358fe90367a1281514a768909bb11d0676d
Aaron.Olds <aaron.olds@gmail.com>
2023-10-04 12:16:48 +0000

    Added exam prep question

---
+++ exam-1.py
@@ -0,0 +1,44 @@
+
+from vpython import *
+
+scene.background = vector(1, 1, 0.8)
+
+# Objects
+
+leftcliff = box(pos=vector(-100, 0, 0), size=vector(2000, 400, 0), color=color.white)
+
+skateboard = box(pos=vector(-100, 250, 0), size=vector(50, 50, 0), color=color.green)
+
+rightcliff = box(pos=vector(1500, 0, 0), size=vector(500, 400, 0), color=color.white)
+
+# Parameters and Initial Conditions
+
+g = vector(0, -9.81, 0)
+
+skateboardm = 1500
+
+skateboardv = vector(10, 0, 0)
+
+skateboardp = skateboardm*skateboardv
+
+# Time and time step
+t = 0
+
+dt = .01
+
+# Calculation Loop
+while skateboard.pos.x <= 875:
+    rate(500)
+
+    Fgrav = skateboardm*g
+
+    Fground = Fgrav
+
+    Fnet = Fgrav-Fground
+
+
+    skateboardp = skateboardp + Fnet*dt
+
+    skateboard.pos = skateboard.pos + (skateboardp/skateboardm)*dt
+
+    t = t + dt
