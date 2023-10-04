3e7e8bf9fc1f1ea099d7a23a7c71b03edf2077ef
Aaron Olds <aolds@sharperimageonline.com>
2023-10-04 08:28:15 -0400

    Fixed program

--- exam-1.py
+++ exam-1.py
@@ -1,15 +1,17 @@
-
 from vpython import *
 
 scene.background = vector(1, 1, 0.8)
 
 # Objects
 
-leftcliff = box(pos=vector(-100, 0, 0), size=vector(2000, 400, 0), color=color.white)
+leftcliff = box(pos=vector(-100, 0, 0),
+                size=vector(2000, 400, 0), color=color.white)
 
-skateboard = box(pos=vector(-100, 250, 0), size=vector(50, 50, 0), color=color.green)
+skateboard = box(pos=vector(-100, 250, 0),
+                 size=vector(50, 50, 0), color=color.green)
 
-rightcliff = box(pos=vector(1500, 0, 0), size=vector(500, 400, 0), color=color.white)
+rightcliff = box(pos=vector(1500, 0, 0), size=vector(
+    500, 400, 0), color=color.white)
 
 # Parameters and Initial Conditions
 
@@ -19,7 +21,9 @@ skateboardm = 1500
 
 skateboardv = vector(10, 0, 0)
 
-skateboardp = skateboardm*skateboardv
+skateboardp = vector(skateboardm * skateboardv.x,
+                     skateboardm * skateboardv.y, skateboardm * skateboardv.z)
+
 
 # Time and time step
 t = 0
@@ -30,14 +34,14 @@ dt = .01
 while skateboard.pos.x <= 875:
     rate(500)
 
-    Fgrav = skateboardm*g
+    Fgrav = vector(skateboardm * g.x, skateboardm * g.y, skateboardm * g.z)
 
     Fground = Fgrav
 
-    Fnet = Fgrav-Fground
+    Fnet = vector(Fgrav.x - Fground.x, Fgrav.y -
+                  Fground.y, Fgrav.z - Fground.z)
 
-
-    skateboardp = skateboardp + Fnet*dt
+    skateboardp = skateboardp + vector(Fnet.x * dt, Fnet.y * dt, Fnet.z * dt)
 
     skateboard.pos = skateboard.pos + (skateboardp/skateboardm)*dt
 
